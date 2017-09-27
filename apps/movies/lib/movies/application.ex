6c93a0b218f3198f152b7984cc1698fda6bb8544
Uk Chukundah <ukchukundah@gmail.com>
2017-09-27 11:13:06 +0100

    Seed cinemas on start (unless we're testing)

--- apps/movies/lib/movies/application.ex
+++ apps/movies/lib/movies/application.ex
@@ -26,6 +26,14 @@ defmodule Moview.Movies.Application do
 
     opts = [strategy: :one_for_one, name: {:global, Moview.Movies.Supervisor}]
 
-    Supervisor.start_link(children, opts)
+    case Supervisor.start_link(children, opts) do
+      {:ok, _} = res ->
+        if Mix.env != :test do
+          Cinema.seed()
+        end
+        res
+      {:error, _} = res ->
+        res
+    end
   end
 end
