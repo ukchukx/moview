4f0a51433ad68d9db00460145215ac171dd9ab7b
Uk Chukundah <ukchukundah@gmail.com>
2017-09-27 12:14:33 +0100

    Add cinemas page.

--- apps/web/lib/web/controllers/page_controller.ex
+++ apps/web/lib/web/controllers/page_controller.ex
@@ -1,8 +1,11 @@
 defmodule Moview.Web.PageController do
   use Moview.Web, :controller
 
+  alias Moview.Movies.{Cinema, Movie, Schedule}
+
   def movies(conn, _params) do
-    render conn, "movies.html"
+    {:ok, _movies} = Movie.get_movies()
+    render conn, "movies.html", title: site_name()
   end
 
   def movie(conn, %{"slug" => _slug} = _) do
@@ -10,6 +13,9 @@ defmodule Moview.Web.PageController do
   end
 
   def cinemas(conn, _) do
-    render conn, "movies.html"
+    {:ok, cinemas} = Cinema.get_cinemas()
+    render conn, "cinemas.html", cinemas: cinemas, title: "Cinemas | #{site_name()}"
   end
+
+  defp site_name, do: "Moview"
 end
