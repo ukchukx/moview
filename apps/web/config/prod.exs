59b4c46034e30b96679f172afbfcfdc1542c5eaa
Uk Chukundah <ukchukundah@gmail.com>
2017-09-29 07:51:52 +0100

    Set host & port
    We'll be running behind an Nginx proxy.

--- apps/web/config/prod.exs
+++ apps/web/config/prod.exs
@@ -15,7 +15,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :web, Moview.Web.Endpoint,
   load_from_system_env: true,
-  url: [host: "example.com", port: 80],
+  url: [host: "localhost", port: System.get_env("MOVIEW_PORT")],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
