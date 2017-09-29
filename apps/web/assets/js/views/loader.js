f49a8b196f63deefc7dff2ef9fe4dc73e62d744c
Uk Chukundah <ukchukundah@gmail.com>
2017-09-29 13:46:38 +0100

    Add movie  view js

--- apps/web/assets/js/views/loader.js
+++ apps/web/assets/js/views/loader.js
@@ -1,9 +1,11 @@
 import MainView from './main';
+import PageMovieView from './page/movie';
 import PageMoviesView from './page/movies';
 
 // Collection of specific view modules
 const views = {
-  PageMoviesView
+  PageMoviesView,
+  PageMovieView
 };
 
 export default function loadView(viewName) {
