783b3cac4a148106faa671eba6600326743da288
Uk Chukundah <ukchukundah@gmail.com>
2017-10-05 16:38:49 +0100

    Override NPM's Bootstrap shenanigans

--- build_prod.sh
+++ build_prod.sh
@@ -4,6 +4,9 @@ source .env
 mix deps.get
 cd apps/web/assets
 npm install
+# After NPM has finished doing its own thing, kindly install the right version of Bootstrap
+npm un bootstrap
+npm i -S bootstrap@4.0.0-alpha.6
 # Place our custom css in bootstrap's domain
 echo '$purple: #683bb7; $brand-primary: $purple; $font-family-sans-serif: Lato, "Helvetica Neue", Helvetica, sans-serif; $font-family-base: $font-family-sans-serif;' >> node_modules/bootstrap/scss/_custom.scss
 ./node_modules/brunch/bin/brunch b -p
