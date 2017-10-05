78667f3ecc365e854d5ea024a3c1b71bdb6efbde
Uk Chukundah <ukchukundah@gmail.com>
2017-10-05 02:27:12 +0100

    Set port value in config

--- apps/web/lib/web/endpoint.ex
+++ apps/web/lib/web/endpoint.ex
@@ -48,7 +48,7 @@ defmodule Moview.Web.Endpoint do
   """
   def init(_key, config) do
     if config[:load_from_system_env] do
-      port = System.get_env("MOVIEW_PORT") || raise "expected the PORT environment variable to be set"
+      port = Application.get_env(:web, :port) || raise "expected the PORT environment variable to be set"
       {:ok, Keyword.put(config, :http, [:inet6, port: port])}
     else
       {:ok, config}
