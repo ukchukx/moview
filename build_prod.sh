f66c8ce6c7a88f53dcbbe762533a69f6bd974e92
Uk <ukchukundah@gmail.com>
2017-10-09 13:02:16 +0100

    Bump version. Rebuild node-sass

--- build_prod.sh
+++ build_prod.sh
@@ -3,6 +3,8 @@ source .env
 mix deps.get
 cd apps/web/assets
 npm install
+# Rebuild node-sass
+npm rebuild node-sass
 # After NPM has finished doing its own thing, kindly install the right version of Bootstrap
 npm un bootstrap
 npm i -S bootstrap@4.0.0-alpha.6
