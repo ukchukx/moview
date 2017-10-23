fdb08990199bf86856ac99179b787aa0601801d8
Uk <ukchukundah@gmail.com>
2017-10-23 04:08:07 +0100

    Bumped versions

--- apps/web/mix.exs
+++ apps/web/mix.exs
@@ -4,7 +4,7 @@ defmodule Moview.Web.Mixfile do
   def project do
     [
       app: :web,
-      version: "0.1.0",
+      version: "1.0.0",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
