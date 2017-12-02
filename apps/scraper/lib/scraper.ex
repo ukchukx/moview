48664c004985743ed3a7a4d2027341f4df5ee8ed
Uk <ukchukundah@gmail.com>
2017-12-02 23:54:00 +0100

    Handle cinema schedules individually

--- apps/scraper/lib/scraper.ex
+++ apps/scraper/lib/scraper.ex
@@ -1,24 +1,25 @@
 defmodule Moview.Scraper do
   require Logger
   alias Moview.Movies.{Movie, Schedule}
+  alias Moview.Scraper.{Genesis, Filmhouse}
 
   def scrape do
-    %{delete: deletion_candidates, create: new_schedule_params} =
-      [Moview.Scraper.Genesis,
-       Moview.Scraper.Filmhouse,
-       Moview.Scraper.Silverbird]
-      |> Enum.map(&(&1.scrape()))
-      |> List.flatten
-      |> Enum.reduce(%{delete: [], create: []}, fn map, acc ->
-        deletes = acc.delete ++ map.delete
-        creates = acc.create ++ map.create
-        %{delete: deletes, create: creates}
-      end)
+    [Genesis, Filmhouse]
+    |> Enum.each(fn module ->
+      %{delete: deletion_list, create: creation_list} =
+        module.scrape
+        |> List.flatten
+        |> Enum.reduce(%{delete: [], create: []}, fn map, acc ->
+          deletes = acc.delete ++ map.delete
+          creates = acc.create ++ map.create
+          %{delete: deletes, create: creates}
+        end)
 
-    Logger.info "Done scraping. Deleting stale schedules..."
-    Enum.each(deletion_candidates, &Schedule.delete_schedule/1)
-    Logger.info "Done deleting stale schedules. Creating new schedules..."
-    Enum.each(new_schedule_params, &Schedule.create_schedule/1)
+      Logger.info "Deleting stale schedules..."
+      Enum.each(deletion_list, &Schedule.delete_schedule/1)
+      Logger.info "Creating new schedules..."
+      Enum.each(creation_list, &Schedule.create_schedule/1)
+    end)
 
     # Remove movies without schedules
     Logger.info "Will now remove movies without schedules..."
